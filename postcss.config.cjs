d00a29562d538d85e2330637aa7787bda13b22cd
StJohn <stjohn@me.com>
2026-02-27 13:14:29 +0000

    Prep for Astro 6

--- postcss.config.cjs
+++
@@ -1,7 +0,0 @@
-const postcssJitProps = require('postcss-jit-props');
-const OpenProps = require('open-props');
-// const postcssNesting = require("postcss-nesting");
-
-module.exports = {
-  plugins: [postcssJitProps(OpenProps)],
-};
